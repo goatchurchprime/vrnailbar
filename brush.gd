3e08853839dfdb026da5c742b083f40de69edaf8
Julian Todd <julian@goatchurch.org.uk>
2024-08-09 13:51:14 +0100

    trying to change angle of the brush by button

--- brush.gd
+++ brush.gd
@@ -4,11 +4,9 @@ extends Node3D
 @onready var paintplane = get_node("/root/Main/ViewportMesh")
 @onready var viewportbrush = get_node("/root/Main/SubViewport/BrushPaint")
 
-func _ready():
-	pass # Replace with function body.
 
 func _process(delta):
-	var brushtip = global_transform.origin
-	var brushtail = global_transform.origin + global_transform.basis.z*0.1
+	var brushtip = $BrushActual.global_transform.origin
+	var brushtail = $BrushActual.global_transform.origin + $BrushActual.global_transform.basis.z*0.1
 	var planeinv = paintplane.global_transform.affine_inverse()
 	viewportbrush.brushpos(planeinv*brushtip, planeinv*brushtail)
